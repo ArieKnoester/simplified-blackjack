27c27d9c4337877f9be4c2d3a85bf1a82fd0b9e4
Arie Knoester <arikstr@gmail.com>
2023-09-01 13:04:47 -0500

    Moved 'cards' list to Player class as a constant

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import random
 import os
 from art import logo
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
 
 def clear_screen():
     if os.name == 'posix':
--- player.py
+++ player.py
@@ -1,3 +1,5 @@
+CARDS = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
 
 class Player:
     def __init__(self):
