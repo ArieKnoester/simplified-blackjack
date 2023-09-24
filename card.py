74b28a020e922f61b01680738a39b6e90b8eeea4
Arie Knoester <arikstr@gmail.com>
2023-09-24 15:53:49 -0500

    Added CardColor data class and attribute.

--- card.py
+++ card.py
@@ -26,10 +26,22 @@ class CardRank(Enum):
     KING = "king"
 
 
+class CardColor(Enum):
+    RED = "red"
+    BLACK = "black"
+
+
 @dataclass
 class Card:
     suit: CardSuit
     rank: CardRank
+
+    @property
+    def color(self) -> CardColor:
+        if self.suit in [CardSuit.HEARTS, CardSuit.DIAMONDS]:
+            return CardColor.RED
+        elif self.suit in [CardSuit.CLUBS, CardSuit.SPADES]:
+            return CardColor.BLACK
 
 
 def all_cards() -> list[Card]:
