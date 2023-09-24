aa8b96eaef34deae3cd849fec436fa222168a31c
Arie Knoester <arikstr@gmail.com>
2023-09-24 16:45:50 -0500

    Added deal_card() to Deck and unit test.

--- deck.py
+++ deck.py
@@ -1,9 +1,11 @@
-from card import all_cards
+from card import Card, all_cards
 import random
 
 
 class Deck:
-
+    """A face down deck of cards:
+    The Card in the first position (0) is the bottom Card.
+    The Card in the last position (-1) is the top Card."""
     def __init__(self):
         self.cards = all_cards()
 
@@ -12,3 +14,6 @@ class Deck:
 
     def shuffle(self):
         random.shuffle(self.cards)
+
+    def deal_card(self) -> Card:
+        return self.cards.pop()
--- tests/test_deck.py
+++ tests/test_deck.py
@@ -9,3 +9,13 @@ def test_deck():
     breakpoint()
     assert actual_len == expected_len
     assert all([isinstance(card, Card) for card in deck.cards])
+
+
+def test_deal_card():
+    deck = Deck()
+    expected_card = deck.cards[-1]
+    actual_card = deck.deal_card()
+    assert expected_card == actual_card
+    assert expected_card is actual_card
+    assert len(deck.cards) == 51
+    assert deck.cards[-1] != actual_card
